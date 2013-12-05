f75f557545f1c0d21951dee204986af36d21951b
Sravanti Tekumalla <stekumal@wellesley.edu>
2013-12-04 23:10:57 -0500

    Added RREF Method
    Changed Matrix infrastructure too

--- MatrixCalculator.java
+++ MatrixCalculator.java
@@ -1,10 +1,17 @@
 public class MatrixCalculator {
+  
   private Matrix matrix;
   
   public MatrixCalculator(int m, int n)
   {
     matrix = new Matrix(m,n); 
-  }
+    for(int i = 0; i < 3; i++) {
+      matrix.setEntry(0, i, 3);
+    }
+    matrix.setEntry(1,1,14);
+    matrix.setEntry(1,2,6);
+    matrix.setEntry(2,2,20);
+   }
   
   public Matrix inverse()
   {
@@ -15,16 +22,39 @@ public class MatrixCalculator {
   public Matrix RREF()
   {
     Matrix temp = matrix.clone();
+    double[][] matrix = temp.getMatrix();
+    for(int i = 0; i < matrix.length; i++) { // removes zero rows, if any, from matrix
+      if(temp.isRowZeroes(i)) temp.removeRow(i); 
+    }
+    for(int i = 0; i < matrix[0].length; i++) {
+      if(!temp.isColumnZeroes(i)) { // ensures column is not all zeroes
+        temp.swapRows(i, temp.getHighest(i)); // swaps ith row with row with highest first elt
+        int index = temp.getNonZero(i);
+        temp.scaleRow(i, matrix[i][index]); // scales the ith row by value of first element
+      }
+    }
+    for(int i = 0; i < matrix[0].length-1; i++) {
+      temp.scaleAll(i);
+    }
     return temp;
   }
   
   public Matrix CREF()
   {
-    Matrix temp = RREF();
+    Matrix temp = matrix.transpose();
+    temp = RREF();
     temp.transpose();
     return temp;
   }
   
-  
+  public static void main(String[] args) {
+    MatrixCalculator mc = new MatrixCalculator(3,3);
+    System.out.println("ORIGINAL MATRIX\n" + mc.matrix);
+
+    System.out.println(mc.RREF());
+    
+    //Matrix reduced = mc.RREF();
+    
+  } 
   
 }
